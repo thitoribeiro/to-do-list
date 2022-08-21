b0dd1773b8f72015ff6d079fedf13638d537698c
Thito Ribeiro <thito.ribeiro@hotmail.com>
2022-08-21 12:26:53 -0300

    Simple get request and nodemon install

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('<h1>My task List</h1>');
+})
+
+app.listen(3000, () => {
+    console.log('Server started');
+})
