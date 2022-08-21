467fdadb05c182b37b178ea6d233720b6fbac043
Thito Ribeiro <thito.ribeiro@hotmail.com>
2022-08-21 13:24:55 -0300

    Simple get request returning a json

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     res.send('<h1>My task List</h1>');
 })
 
+app.get('/json', (req, res) => {
+    res.json({title: 'Task X', done: true});
+})
+
 app.listen(3000, () => {
     console.log('Server started');
 })
