347712d77ed1aff91d6c12fc2c838ee03e446878
Thito Ribeiro <thito.ribeiro@hotmail.com>
2022-09-10 12:13:18 -0300

    Get by Id route with success and error message

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -6,16 +6,24 @@ const Checklist = require('../models/checklist');
 
 router.get('/', async (req, res) => {
     try {
-        let checklist = await Checklist.find({});
-        res.status(200).send(checklist);
-    } catch {
+        let checklists = await Checklist.find({});
+        res.status(200).send(checklists);
+    } catch (error) {
         res.status(500).json(error)
     }
 })
 
-router.get('/:id', (req, res) => {
-    console.log(req.params.id);
-    res.send(`ID: ${req.params.id}`);
+router.get('/:id', async (req, res) => {
+
+    try {
+        let checklist = await Checklist.findById(req.params.id);
+        res.status(200).send(checklist);
+
+    } catch (error) {
+        res.status(422).json(error);
+    }
+
+
 })
 
 router.post('/', async (req, res) => {
